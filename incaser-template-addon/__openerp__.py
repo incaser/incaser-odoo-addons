1a6d5be675b8143056597a947a2f8d22b9eb6035
sergio-incaser <sergio@incaser.es>
2015-09-11 09:01:47 +0200

    [ADD] incaser-template-addon: Template addon OCA - Incaser.

---
+++ incaser-template-addon/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# (c) 2015 Incaser Informatica S.L. - Sergio Teruel
+# (c) 2015 Incaser Informatica S.L. - Carlos Dauden
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': '....',
+    'summary': '.....'
+               '.....',
+    'version': '8.0.1.0.0',
+    'category': "......",
+    'author': 'Incaser Informatica S.L., '
+              'Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'depends': [],
+    'data': [],
+    'installable': True,
+}
