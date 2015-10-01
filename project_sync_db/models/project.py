6238086468216b9f908139ad785418877bda0f7a
sergio-incaser <sergio@incaser.es>
2015-10-01 11:26:37 +0200

    [DEV] project_sync_db: Module development.

---
+++ project_sync_db/models/project.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# (c) 2015 Incaser Informatica S.L. - Sergio Teruel
+# (c) 2015 Incaser Informatica S.L. - Carlos Dauden
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from openerp import models, fields
+
+
+class ProjectTaskType(models.Model):
+    _inherit = 'project.project'
+
+    external_project_id = fields.Integer(
+        string='External Project ID')
+
+
+class ProjectTaskType(models.Model):
+    _inherit = 'project.task'
+
+    external_task_id = fields.Integer(
+        string='External Task ID')
