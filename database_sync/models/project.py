4758f5bf8504e6e0daa70557eb1767b48593a32c
sergio-incaser <sergio@incaser.es>
2015-11-04 12:03:56 +0100

    [DEV] database_sunc: add external user id into task

--- database_sync/models/project.py
+++ database_sync/models/project.py
@@ -18,4 +18,6 @@ class ProjectTask(models.Model):
 
     external_task_id = fields.Integer(
         string='External Task ID')
+    external_user_id = fields.Integer(
+        string='External Task User ID')
 
