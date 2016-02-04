893fbbbf8924fc7ab20b5cc339080106ed1f0647
Sergio Incaser <sergio@incaser.es>
2016-02-04 18:02:13 +0100

    [IMP][8.0] portal_select_all: New module

---
+++ portal_select_all/wizard/portal_wizard.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# (c) 2015 Incaser Informatica S.L. - Sergio Teruel
+# (c) 2015 Incaser Informatica S.L. - Carlos Dauden
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from openerp import api, fields, models
+
+
+class PortalWizard(models.AbstractModel):
+    _inherit = 'portal.wizard'
+
+    select_all = fields.Boolean()
+    modified = fields.Boolean()
+
+    @api.onchange('select_all')
+    def _onchange_select_all(self):
+        # The variable modified avoids that all records set false at load view
+        if self.modified:
+            for user in self.user_ids:
+                user.in_portal = self.select_all
+        self.modified = True
