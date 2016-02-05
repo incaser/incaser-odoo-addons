acad398a4b118177d4a1ce75cb121002b2f240c4
Sergio Incaser <sergio@incaser.es>
2016-02-05 18:07:27 +0100

    [NEW][8.0] report_header_full_width

--- report_header_full_width/__openerp__.py
+++ report_header_full_width/__openerp__.py
@@ -4,16 +4,18 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    'name': '....',
-    'summary': '.....'
-               '.....',
+    'name': 'Report Header Full Width',
+    'summary': 'Full width for reports header',
     'version': '8.0.1.0.0',
-    'category': "......",
     'author': 'Incaser Informatica S.L., '
               'Odoo Community Association (OCA)',
     'license': 'AGPL-3',
-    'depends': [],
-    'data': [],
-    'images':[],
+    'website': 'http://www.incaser.es',
+    'category': 'Tools',
+    'depends': ['report'],
+    'data': [
+            'views/layouts.xml',
+             ],
+    'images': [],
     'installable': True,
 }
