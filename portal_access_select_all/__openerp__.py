29f81e043d2b6db7412dbb8763d9bb3212addccb
Sergio Incaser <sergio@incaser.es>
2016-02-09 10:36:11 +0100

    [IMP][8.0] portal_access_select_all: Rename module

---
+++ portal_access_select_all/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# (c) 2015 Incaser Informatica S.L. - Sergio Teruel
+# (c) 2015 Incaser Informatica S.L. - Carlos Dauden
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': 'Portal Access Select All',
+    'summary': 'This module allows select all users in portal wizard',
+    'version': '8.0.1.0.0',
+    'category': 'Tools',
+    "website": "https://odoo-community.org/",
+    'author': 'Incaser Informatica S.L., '
+              'Odoo Community Association (OCA)',
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "portal",
+    ],
+    'data': [
+        'wizards/portal_wizard_view.xml',
+     ],
+}
